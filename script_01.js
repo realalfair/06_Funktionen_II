b0de48f97e888fafe1fb4e8a3f1ee49054f2461b
realalfair <irina.merzdorf@gmail.com>
2023-07-18 10:14:18 +0200

    02 / addition

--- script_01.js
+++ script_01.js
@@ -8,11 +8,22 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
 
 
 
+
+
+
+
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a+b;
+}
 
 
 // module: output | test:
