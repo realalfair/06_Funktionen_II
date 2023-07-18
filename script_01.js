ee874e0517fe734b86056902c10bd4035e21cbdc
realalfair <irina.merzdorf@gmail.com>
2023-07-18 11:17:35 +0200

    module: division a / b

--- script_01.js
+++ script_01.js
@@ -11,12 +11,44 @@
 4. Ausgabe in Konsole : check!
 */
 
+const ERROR_STR_DIV = "Division durch 0 nicht möglich"
+
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+function divide(a,b) {
+
+	if (b == 0) { // Ausnahme + Abbruch
+		return ERROR_STR_DIV;
+	}
+	// if (b != 0) {
+	// 	return a/b;
+	// } else {
+	// 	return "Division durch 0 nicht möglich";
+	// }
+
+	return a/b; //Regel
+
+}
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+    return a*b;
+}
+
 
 // module: subtraction a - b |  test:
-output(subtract(3,2));
-output(subtract(3,-2));
-output(subtract(3,0));
-output(subtract(0,3));
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,3));
 function subtract(a,b) {
 	return a - b;
 }
